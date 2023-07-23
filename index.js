98f618859520182c0a1b365bcd921d962dfb1c12
oscar garcia <og553091@gmail.com>
2023-07-23 18:51:50 -0500

    finished readme generator

---
+++ index.js
@@ -0,0 +1,109 @@
+const fs = require("fs");
+const  inquirer =  require("inquirer");
+const path = require("path");
+const genertateMarkdown = require("./utils/generateMarkdown");
+
+
+const questions = [
+
+{
+type:"input",
+name:"title",
+message:"What is the name of your project .", 
+},
+{
+type:"input",
+name:"Description",
+message: "please describe the functionality of your project",
+
+},
+{
+type:"input",
+name:"link",
+message:"please enter a url where a user can see your deployed application .",
+},
+{
+            type:"list",                                    
+            name:"license",
+            message:"please select a license applicable to this application .",
+            choices:["MIT","BSD2","none"]
+    },
+    {
+        type:"input",
+        name:"require",
+        message:"list any dependencies used for this project .",
+        },
+ {
+ type:"input",
+ name:"features",
+ message:"list some features about this project that you would want the user to know .",
+  },
+  {
+ type:"input",
+ name:"usage",
+     message:"please list the technologies used for this project .",
+     },
+     {
+        type:"input",
+        name:"creator",
+        message:"write your github username here .",
+        },
+        
+        {
+            type:"input",
+            name:"email",
+            message:"please provide a valid email here  .",
+            },
+            {
+                type:"input",
+                name:"contributors",
+                message:"please list any contributors here (use github usernames) .",
+                },
+                  {
+type : 'input',
+name: 'installation',
+message:'please provide any installation steps required'
+
+ } ,
+{
+type:'input',
+name:'tests',
+message:'please provide any tests used on this project'
+
+
+}               ];
+
+
+
+function writeToFile (fileName, data){
+return fs.writeFileSync(path.join(process.cwd(),fileName),data);
+
+
+}
+function init(){
+inquirer.prompt(questions).then((responses)=>{
+writeToFile("./dist/README.md",genertateMarkdown({...responses}));
+
+
+});
+
+}
+init();
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,244 @@
+// function renderLicenseBadge(license) {
+//     if (license !== "none") {
+//       return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+//     }
+//     return "";
+//   }
+//   function genertateReadMe(data) {
+//     return `# ${data.title} 
+//   ${renderLicenseBadge(data.license)}
+//   ## Description
+//   ${data.description}
+//   ## Deployed Application URL
+//   ${data.link}
+  
+//   ## Table of Contents
+//   * [Features](#features)
+//   * [Languages & Dependencies](#languagesanddependencies)
+//   * [How to Use This Application](#HowtoUseThisApplication)
+//   * [Contributors](#contributors)
+//   * [Testing](#testing)
+//   * [Questions](#questions)
+//   ## Features
+//   ${data.features}
+//   ## Languages & Dependencies
+//   ${data.require}
+//   ## How to Use This Application:
+//   ${data.usage}
+//   ## Contributors
+//   ${data.contributors}
+  
+//   ## Questions
+//   Please send your questions [here](mailto:${data.email}?subject=[GitHub]%20Dev%20Connect) or visit [github/${data.creator}](https://github.com/${data.creator}).
+//   `;
+//   }
+//   module.exports = genertateReadMe;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function renderLicenseBadge(license) {
+//   if (license !== "none") {
+//     return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+//   }
+//   return "";
+// }
+
+// //Creating a function to generate markdown for README
+// function generateMarkdown(data) {
+//   return `# ${data.title} 
+// ${renderLicenseBadge(data.license)}
+// ## Description
+// ${data.description}
+// ## Deployed Application URL
+// ${data.link}
+// ## Screenshot
+// ![alt-text](${data.screenshot})
+// ## Table of Contents
+// * [Features](#features)
+// * [Languages & Dependencies](#languagesanddependencies)
+// * [How to Use This Application](#HowtoUseThisApplication)
+// * [Contributors](#contributors)
+// * [Testing](#testing)
+// * [Questions](#questions)
+// ## Features
+// ${data.features}
+// ## Languages & Dependencies
+// ${data.require}
+// ## How to Use This Application:
+// ${data.usage}
+// ## Contributors
+// ${data.contributors}
+// ## Testing
+// ${data.test}
+// ## Questions
+// Please send your questions [here](mailto:${data.email}?subject=[GitHub]%20Dev%20Connect) or visit [github/${data.creator}](https://github.com/${data.creator}).
+// `;
+// }
+
+// module.exports = generateMarkdown;
+
+
+
+
+
+
+
+
+
+
+
+
+
+function renderLicenseBadge(license) {
+  if (license === 'Apache') {
+    return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]'
+  } else if (license === 'Boost') {
+    return '[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)]'
+  }
+  else if (license === 'BSD') {
+    return '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]'
+  }
+  else if (license === 'Creative Commons') {
+    return '[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)]'
+  }
+  else if (license === 'Eclipse') {
+    return '[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)]'
+  }
+  else if (license === 'GNU') {
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]'
+  }
+  else if (license === 'IBM') {
+    return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)]'
+  } else if (license === 'ISC') {
+    return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]'
+  } else if (license === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]'
+  } else if (license === 'Mozilla') {
+    return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)]'
+  }
+  else if (license === 'Perl') {
+    return '[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)]'
+  } else if (license === 'SIL') {
+    return '[![License: Open Font-1.1](https://img.shields.io/badge/License-OFL%201.1-lightgreen.svg)]'
+  } else if (license === 'Unlicense') {
+    return '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]'
+  } else if (license === 'WTFPL') {
+    return '[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)]'
+  } else if (license === 'Zlib') {
+    return '[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)]'
+  } else {
+    return ''
+  }
+}
+
+function renderLicenseLink(license) {
+
+  if (license === 'Apache') {
+    return '(https://opensource.org/licenses/Apache-2.0)'
+  }
+  else if (license === 'Boost') {
+    return '(https://www.boost.org/LICENSE_1_0.txt)'
+  }
+  else if (license === 'BSD') {
+    return '(https://opensource.org/licenses/BSD-3-Clause)'
+  }
+  else if (license === 'Creative Commons') {
+    return '(http://creativecommons.org/publicdomain/zero/1.0/)'
+  }
+  else if (license === 'Eclipse') {
+    return '(https://opensource.org/licenses/EPL-1.0)'
+  }
+  else if (license === 'GNU') {
+    return '(https://www.gnu.org/licenses/gpl-3.0)'
+  }
+  else if (license === 'IBM') {
+    return '(https://opensource.org/licenses/IPL-1.0)'
+  }
+  else if (license === 'ISC') {
+    return '(https://opensource.org/licenses/ISC)'
+  }
+  else if (license === 'MIT') {
+    return '(https://opensource.org/licenses/MIT)'
+  }
+  else if (license === 'Mozilla') {
+    return '(https://opensource.org/licenses/MPL-2.0)'
+  }
+  else if (license === 'Perl') {
+    return '(https://opensource.org/licenses/Artistic-2.0)'
+  }
+  else if (license === 'SIL') {
+    return '(https://opensource.org/licenses/OFL-1.1)'
+  }
+  else if (license === 'Unlicense') {
+    return '(http://unlicense.org/)'
+  }
+  else if (license === 'WTFPL') {
+    return '(http://www.wtfpl.net/about/)'
+  }
+  else if (license === 'Zlib') {
+    return '(https://opensource.org/licenses/Zlib)'
+  }
+  else {
+    return ''
+  }
+
+}
+
+function renderLicenseSection(license) {
+  return `${renderLicenseBadge(license)}${renderLicenseLink(license)}`
+}
+
+function generateMarkdown(data) {
+  console.log(data)
+  return `
+  # ${data.title}
+
+  ## License
+  ${renderLicenseSection(data.license)}
+
+  ## Description
+  ${data.Description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Main Features](#main-features)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+  
+  ## Main Features
+  ${data.features}
+
+  ## Contributing
+  ${data.contributors}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  If you have any questions, please contact me at ${data.email} or visit my [GitHub page](https://github.com/${data.creator})
+  
+`;
+}
+
+module.exports = generateMarkdown;
